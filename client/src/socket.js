b5cecdf76a8cb1192aea1e8fd0befa174363afef
SuvayuN <suvayuindia@gmail.com>
2026-04-27 14:14:44 +0530

    FrontendFix

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Create a single shared socket instance for the entire app
-const socket = io('http://localhost:5000', {
+const socket = io(`${process.env.REACT_APP_SOCKET_URL}`, {
     transports: ['websocket'],
     reconnection: true,
     reconnectionDelay: 1000,
